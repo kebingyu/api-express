3ea40c634ce4ba4df93f0f92e78b32b0137fb146
kebingyu <yukebing@gmail.com>
2015-09-14 16:33:24 -0700

    clean up

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var validator = require('../services/validator');
-var userModel = require('../models/user');
 var UserModel = require('../models/User');
 
 router.use(function (req, res, next) {
